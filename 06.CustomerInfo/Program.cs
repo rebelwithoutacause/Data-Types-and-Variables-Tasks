19631ff2938ae7da44ba9405875fed9d20f7bc04
rebelwithoutacause <148477548+rebelwithoutacause@users.noreply.github.com>
2025-02-26 14:04:38 +0200

    Add files via upload

---
+++ 06.CustomerInfo/Program.cs
@@ -0,0 +1,4 @@
+﻿string firstName = Console.ReadLine();
+string secondName = Console.ReadLine();
+string email = Console.ReadLine();
+Console.WriteLine($"Customer: {firstName} {secondName} ({email})");
