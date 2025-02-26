cc8da5e934780826008380a67b9b30f40f0796e0
rebelwithoutacause <148477548+rebelwithoutacause@users.noreply.github.com>
2025-02-26 14:03:07 +0200

    Add files via upload

---
+++ 04.Calculations/Program.cs
@@ -0,0 +1,6 @@
+﻿int firstResult = 5 + 3 * 2;
+Console.WriteLine(firstResult);
+int secondResult = 4 * (2 + 3);
+Console.WriteLine(secondResult);
+int thirdResult = (2 + 5)  *  (8 - 2)  / 7;
+Console.WriteLine(thirdResult);
